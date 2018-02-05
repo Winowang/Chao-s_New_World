84cf01f7af160204f986a8c18582bbc58702a4cb
Chao Wang <33717080+Winowang@users.noreply.github.com>
2018-02-05 20:44:21 +0800

    Update Loading.py

--- DL_AI/Loading.py
+++ DL_AI/Loading.py
@@ -1,10 +1,8 @@
 """
-mnist_loader
+Loading
 ~~~~~~~~~~~~
-A library to load the MNIST image data.  For details of the data
-structures that are returned, see the doc strings for ``load_data``
-and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
-function usually called by our neural network code.
+A library to load the MNIST image data.
+``load_data_wrapper`` is the function called by our neural network code.
 """
 
 #### Libraries
@@ -16,24 +14,14 @@ import gzip
 import numpy as np
 
 def load_data():
-    """Return the MNIST data as a tuple containing the training data,
+    """A tuple containing the training data,
     the validation data, and the test data.
     The ``training_data`` is returned as a tuple with two entries.
-    The first entry contains the actual training images.  This is a
-    numpy ndarray with 50,000 entries.  Each entry is, in turn, a
-    numpy ndarray with 784 values, representing the 28 * 28 = 784
-    pixels in a single MNIST image.
+    The first entry contains the actual training images.
     The second entry in the ``training_data`` tuple is a numpy ndarray
-    containing 50,000 entries.  Those entries are just the digit
-    values (0...9) for the corresponding images contained in the first
-    entry of the tuple.
+    containing 50,000 entries.
     The ``validation_data`` and ``test_data`` are similar, except
-    each contains only 10,000 images.
-    This is a nice data format, but for use in neural networks it's
-    helpful to modify the format of the ``training_data`` a little.
-    That's done in the wrapper function ``load_data_wrapper()``, see
-    below.
-    """
+    each contains only 10,000 images."""
     f = gzip.open('../data/mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = cPickle.load(f)
     f.close()
